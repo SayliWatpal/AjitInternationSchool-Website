d40347b9d70db80aa272e20a9c440262394c5ec6
SayliWatpal <sayliwatpal2309@gmail.com>
2026-04-04 20:23:40 +0530

    Updated contact form with Google Forms integration and UI improvements

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const menuToggle = document.getElementById("menu-toggle");
 const navbar = document.getElementById("navbar");
 
@@ -21,4 +22,27 @@ links.forEach(link => {
     if(link.getAttribute("href") === currentPage){
         link.classList.add("active");
     }
+});
+
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("entry.24211047", document.getElementById("name").value);
+    formData.append("entry.2049927103", document.getElementById("email").value);
+    formData.append("entry.347530787", document.getElementById("phone").value);
+    formData.append("entry.1152254244", document.getElementById("message").value);
+
+    fetch("https://docs.google.com/forms/d/e/1FAIpQLSc4_5SsG1qRcqVqmfWQdmbyESeXHg0Ujcqqkf1pYyilQOjpMg/formResponse", {
+        method: "POST",
+        body: formData,
+        mode: "no-cors"
+    });
+
+    // Delay to make it feel natural
+    setTimeout(() => {
+    document.getElementById("successMsg").style.display = "block";
+    document.getElementById("contactForm").reset();
+}, 500);
+
 });
